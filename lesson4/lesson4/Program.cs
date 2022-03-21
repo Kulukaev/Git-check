ef2548db96ad41e4834f2b0d3217a0a718a960a0
Kulukaev <hnlmat1@yandex.ru>
2022-03-21 16:47:14 +0300

    4 урок

---
+++ lesson4/lesson4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lesson4
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var ex = new handler();
+
+
+            ex.OnKeyPressed += print;
+            ex.Run();
+
+            void print(Object sender, char letter)
+            {
+                Console.WriteLine($"\nВаш символ:  {letter}");
+            }
+        }
+    }
+}
---
+++ lesson4/lesson4/handler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson4
+{
+    class handler
+    {
+        public event EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            var k = true;
+            while (k)
+            {
+                Console.WriteLine("Нажмите на кнопку:");
+                var letter = Console.ReadKey();
+
+                switch (letter.KeyChar)
+                {
+                    case 'с':
+                        k = false;
+                        break;
+                    case 'c':
+                        k = false;
+                        break;
+
+                    default:
+                        OnKeyPressed?.Invoke(this, letter.KeyChar);
+                        break;
+                }
+
+            }
+        }
+    }
+}
+
