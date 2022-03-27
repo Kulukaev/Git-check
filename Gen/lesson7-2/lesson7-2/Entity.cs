a40c99cff6fdfbdeadb51893a0eb448585bbe6b7
Kulukaev <hnlmat1@yandex.ru>
2022-03-27 18:47:54 +0300

    lesson 7 -2

---
+++ Gen/lesson7-2/lesson7-2/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace lesson7_2
+{
+    class Entity
+    {
+        public int Id;
+        public int ParentId;
+        public string Name;
+        public static Dictionary<int, List<Entity>> ToDictionary(List<Entity> list)
+        {
+            var dict = list
+                .GroupBy(item => item.ParentId)
+                .ToDictionary(g => g.Key, g => g.ToList());
+
+            return dict;
+        }
+    }
+}
